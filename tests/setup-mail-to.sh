f8357c63698c828667ac7b3c747acc619911f0ae
Kai Hendry <kai.hendry@unee-t.com>
2018-09-25 14:25:32 +0800

    e2e dev test completed with test@dev.unee-t.com

--- tests/setup-mail-to.sh
+++ tests/setup-mail-to.sh
@@ -3,28 +3,43 @@
 for STAGE in dev demo prod
 do
 
-	FROM="prod Unee-T case <test@unee-t.com>"
+FROM="prod Unee-T case <test@unee-t.com>"
+
+bugid=62321
+
+CASE=https://case.unee-t.com/case/$bugid
 
 if test "$STAGE" != "prod"
 then
 	FROM="$STAGE Unee-T case <test@$STAGE.unee-t.com>"
+	CASE="https://case.$STAGE.unee-t.com/case/$bugid"
 fi
 
 MAIL_URL=$(aws --profile uneet-${STAGE} ssm get-parameters --names MAIL_URL --with-decryption --query Parameters[0].Value --output text)
+SECRET=$(aws --profile uneet-${STAGE} ssm get-parameters --names API_ACCESS_TOKEN --with-decryption --query Parameters[0].Value --output text)
+
+
+REPLY=reply+$bugid-$(echo -n $bugid | hmac256 $SECRET)@$STAGE.unee-t.com
+if test $STAGE == "prod"
+then
+	REPLY=reply+$bugid-$(echo -n $bugid | hmac256 $SECRET)@unee-t.com
+fi
+
+DATE=$(date)
 
 cat << END > index.$STAGE.js
-var nodemailer = require('nodemailer');
+var nodemailer = require('nodemailer')
 
 // create reusable transporter object using the default SMTP transport
-var transporter = nodemailer.createTransport('${MAIL_URL}');
+var transporter = nodemailer.createTransport('${MAIL_URL}')
 
 // setup e-mail data with unicode symbols
 var mailOptions = {
-    from: '${FROM}>', // sender address
-    to: 'kai.hendry@gmail.com', // list of receivers
-    subject: 'Hello ✔', // Subject line
-    text: 'Hello world ?', // plaintext body
-    html: '<b>Hello world ?</b>' // html body
+	from: '${FROM}', // sender address must be whitelisted for this to work
+	to: '${REPLY}',
+	subject: 'Testing ${DATE}', // Subject line
+	text: 'Text message ${DATE}', // plaintext body
+	html: '<b>Html message ${DATE}</b>' // html body
 };
 
 // send mail with defined transport object
@@ -32,7 +47,8 @@ transporter.sendMail(mailOptions, function(error, info){
     if(error){
         return console.log(error);
     }
-    console.log('Message sent: ' + info.response);
+    console.log('Message $DATE sent: ' + info.response);
+    console.log('Verify on $CASE with $FROM');
 });
 END
 
